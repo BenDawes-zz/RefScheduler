a905403c2a1623a15ae855215e7b4dfa209602e1
Eamonn Harrison <ech2@st-andrews.ac.uk>
2017-01-22 23:54:04 +0000

    Implemented create form for persons.

--- src/web/ref/personController.js
+++ src/web/ref/personController.js
@@ -5,4 +5,32 @@ var myApp = angular.module('refScheduler', []).controller('personCtrl', function
         }, function error(response) {
             $scope.errors = response.status;
         });
+
+        $scope.firstName = '';
+        $scope.lastName = '';
+        $scope.email = '';
+        $scope.hrLevel = '';
+        $scope.arLevel = '';
+        $scope.srLevel = '';
+        $scope.snitch = '';
+
+        $scope.submit = function() {
+          $http({
+            method: 'POST',
+            url: 'http://localhost:8090/person',
+            data: {
+                firstName: $scope.firstName,
+                lastName: $scope.lastName,
+                email: $scope.email,
+                headRefereeLevel: $scope.hrLevel,
+                assistantRefereeLevel: $scope.arLevel,
+                snitchRefereeLevel: $scope.srLevel,
+                snitch: $scope.snitch
+            }
+          }).then(function success(response) {
+                $scope.response = response.data;
+          }, function error(response) {
+                $scope.errors = response.status;
+          });
+        };
 });
