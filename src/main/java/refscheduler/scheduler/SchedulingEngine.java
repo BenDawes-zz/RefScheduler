6bd0932a3794076e2591c67e8ca7cc41ff3adfda
Eamonn Harrison <ech2@st-andrews.ac.uk>
2017-08-18 19:25:56 +0100

    Break down scheduling engine to schedule and check by timeslot separately.

--- src/main/java/refscheduler/scheduler/SchedulingEngine.java
+++ src/main/java/refscheduler/scheduler/SchedulingEngine.java
@@ -1,16 +1,16 @@
 package refscheduler.scheduler;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import refscheduler.affiliation.Affiliation;
 import refscheduler.game.Game;
-import refscheduler.game.GameService;
 import refscheduler.person.Person;
 import refscheduler.timeslot.Timeslot;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static java.util.stream.Collectors.toList;
 
@@ -20,44 +20,72 @@ import static java.util.stream.Collectors.toList;
 @Service("schedulingEngine")
 public class SchedulingEngine {
 
-    @Autowired
-    private GameService gameService;
-
-    public void scheduleGames(final Map<Timeslot, List<Game>> allGames,
+    public Map<Timeslot, List<Game>> scheduleGames(final Map<Timeslot, List<Game>> allGames,
                               final List<Person> persons,
                               final List<Affiliation> affiliations) {
-        for (List<Game> timeslotGames : allGames.values()) {
-            List<Person> usedPeople = new ArrayList<>();
 
-            for (Game game : timeslotGames) {
-                List<Person> unavailablePeople = affiliations.stream()
-                        .filter(x -> game.getTeamA().equals(x.getTeam())
-                        || game.getTeamB().equals(x.getTeam())
-                        || usedPeople.contains(x.getPerson()))
-                        .map(Affiliation::getPerson)
-                        .collect(toList());
+        for (Timeslot timeslot : allGames.keySet()) {
+            allGames.put(timeslot, scheduleTimeslot(allGames.get(timeslot), persons, affiliations));
+        }
 
-                List<Person> potentialPeople = persons.stream()
-                        .filter(x -> !unavailablePeople.contains(x))
-                        .collect(toList());
+        return allGames;
+    }
 
-                if (potentialPeople.size() >= 6) {
-                    game.setHeadReferee(potentialPeople.get(0));
-                    usedPeople.add(potentialPeople.get(0));
-                    game.setAssistantRefereeA(potentialPeople.get(1));
-                    usedPeople.add(potentialPeople.get(1));
-                    game.setAssistantRefereeB(potentialPeople.get(2));
-                    usedPeople.add(potentialPeople.get(2));
-                    game.setAssistantRefereeC(potentialPeople.get(3));
-                    usedPeople.add(potentialPeople.get(3));
-                    game.setSnitchReferee(potentialPeople.get(4));
-                    usedPeople.add(potentialPeople.get(4));
-                    game.setSnitch(potentialPeople.get(5));
-                    usedPeople.add(potentialPeople.get(5));
+    public List<Game> scheduleTimeslot(List<Game> games, List<Person> people, List<Affiliation> affiliations) {
+        List<Person> usedPeople = new ArrayList<>();
 
-                    gameService.save(game);
-                }
+        for (Game game : games) {
+            List<Person> unavailablePeople = affiliations.stream()
+                    .filter(x -> game.getTeamA().equals(x.getTeam())
+                            || game.getTeamB().equals(x.getTeam())
+                            || usedPeople.contains(x.getPerson()))
+                    .map(Affiliation::getPerson)
+                    .collect(toList());
+
+            List<Person> potentialPeople = people.stream()
+                    .filter(x -> !unavailablePeople.contains(x))
+                    .collect(toList());
+
+            if (potentialPeople.size() >= 6) {
+                game.setHeadReferee(potentialPeople.get(0));
+                usedPeople.add(potentialPeople.get(0));
+                game.setAssistantRefereeA(potentialPeople.get(1));
+                usedPeople.add(potentialPeople.get(1));
+                game.setAssistantRefereeB(potentialPeople.get(2));
+                usedPeople.add(potentialPeople.get(2));
+                game.setAssistantRefereeC(potentialPeople.get(3));
+                usedPeople.add(potentialPeople.get(3));
+                game.setSnitchReferee(potentialPeople.get(4));
+                usedPeople.add(potentialPeople.get(4));
+                game.setSnitch(potentialPeople.get(5));
+                usedPeople.add(potentialPeople.get(5));
             }
         }
+
+        return games;
+    }
+
+    public boolean checkTournamentSchedule(Map<Timeslot, List<Game>> gamesToCheck) {
+        boolean scheduleOkay = true;
+
+        for (Timeslot timeslot : gamesToCheck.keySet()) {
+            List<Game> gamesInTimeslot = gamesToCheck.get(timeslot);
+
+            scheduleOkay = scheduleOkay && checkTimeslotSchedule(gamesInTimeslot);
+        }
+
+        return scheduleOkay;
+    }
+
+    public boolean checkTimeslotSchedule(List<Game> games) {
+        Set<Person> affiliatedOfficials = new HashSet<>();
+
+        for (Game game : games) {
+            if (!affiliatedOfficials.addAll(game.getGameOfficials())) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- src/test/java/refscheduler/scheduler/SchedulingEngineTest.java
+++ src/test/java/refscheduler/scheduler/SchedulingEngineTest.java
@@ -1,6 +1,5 @@
 package refscheduler.scheduler;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,19 +12,13 @@ import refscheduler.game.Game;
 import refscheduler.game.GameService;
 import refscheduler.person.Person;
 import refscheduler.person.PersonService;
-import refscheduler.team.Team;
 import refscheduler.team.TeamService;
 import refscheduler.timeslot.Timeslot;
-import refscheduler.timeslot.TimeslotService;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonMap;
-import static refscheduler.game.builder.GameBuilder.newGame;
+import static junit.framework.TestCase.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Main.class)
@@ -41,47 +34,21 @@ public class SchedulingEngineTest {
     private TeamService teamService;
 
     @Autowired
-    private TimeslotService timeslotService;
-
-    @Autowired
     private PersonService personService;
 
     @Autowired
     private AffiliationService affiliationService;
 
-    private Map<Timeslot, List<Game>> games;
-    private List<Person> people;
-    private List<Affiliation> affiliations;
-    private List<Timeslot> timeslots;
-
-    @Before
-    public void setup() {
-        List<Team> teams = teamService.getTeams();
-        timeslots = timeslotService.getAllTimeslots();
-        people = personService.getAll();
-        affiliations = affiliationService.getAllAffiliations();
-
-        // Games
-        Game gameOne = newGame().withTeamA(teams.get(0)).withTeamB(teams.get(1)).withTimeslot(timeslots.get(0)).withPitch(1L).build();
-        Game gameTwo = newGame().withTeamA(teams.get(2)).withTeamB(teams.get(3)).withTimeslot(timeslots.get(0)).withPitch(2L).build();
-
-        games = singletonMap(timeslots.get(0), asList(gameOne, gameTwo));
-    }
-
     @Test
     public void simpleSchedule() {
+        Map<Timeslot, List<Game>> games = gameService.getAllGamesByTimeslot();
+        List<Person> people = personService.getAll();
+        List<Affiliation> affiliations = affiliationService.getAllAffiliations();
+
         schedulingEngine.scheduleGames(games, people, affiliations);
 
-        List<Game> games = gameService.getGames();
+        games = gameService.getAllGamesByTimeslot();
 
-        for (Timeslot timeslot : timeslots) {
-            Set<Person> scheduledOfficials = new HashSet<>();
-
-            games.stream()
-                .filter(game -> timeslot.equals(game.getTimeslot()))
-                .map(game -> scheduledOfficials.addAll(game.getGameOfficials()));
-
-            System.out.println(scheduledOfficials);
-        }
+        assertTrue(schedulingEngine.checkTournamentSchedule(games));
     }
 }
